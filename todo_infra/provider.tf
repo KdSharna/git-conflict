82512d128a3c12f6cc5d503d40a0dd084042089a
kdsharma <sharmakapil79027@gmail.com>
2025-09-21 23:51:00 +0530

    Update provider.tf

--- todo_infra/provider.tf
+++ todo_infra/provider.tf
@@ -8,12 +8,12 @@ terraform {
   }
  backend "azurerm" {
     resource_group_name  = "pondu1"
-    storage_account_name = "tattu20"
+    storage_account_name = "tattu22"
     container_name       = "con99"
     key                  = "kapil.tfstate"
   }
 }
 provider "azurerm" {
   features {}
-  subscription_id = "3be29ba2-b31c-4fbd-a7bb-c51bf50ef740"
+  subscription_id = "85bce315-2d40-4002-a3d1-b7f28202855c"
 }
